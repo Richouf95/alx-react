a9912cc7f50c587dac6330dbf338f9850922fac6
Richouf95 <wourichouf95@gmail.com>
2024-08-15 12:41:46 +0100

    Fix << Converting into an Immutable object using fromJS >>

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,19 +1,9 @@
 // Import Immutable.js library
-const { fromJS } = require('immutable');
+import { fromJS } from 'immutable';
 
 // Function to convert a JS object into an Immutable Map
 function getImmutableObject(object) {
     return fromJS(object);
 }
-
-// Example usage
-const obj = {
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
-};
-
-console.log(getImmutableObject(obj));
 
 module.exports = getImmutableObject;
